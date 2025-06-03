dc3456839b929b5b3d999c80fdaaf042b8210153
syam-ts <syamnandhu3@gmail.com>
2025-06-04 00:54:46 +0530

    fix: dist folder path fix

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,32 @@
 import express from "express";
 import http from "node:http";
-import path from 'node:path';
-import dotenv from 'dotenv';
+import path from "node:path";
+import dotenv from "dotenv";
 dotenv.config({
-    path: path.resolve(__dirname, '../env')
-  });
-import cors from 'cors';
+  path: path.resolve(__dirname, "../env"),
+});
+import cors from "cors";
 import { connectDB } from "./infrastructure/database/db";
-import userRouter from './infrastructure/http/routes/userRouter'
-import cookieparser from 'cookie-parser';
- 
+import userRouter from "./infrastructure/http/routes/userRouter";
+import cookieparser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieparser())
+app.use(cookieparser());
 app.use(
-    cors({
-        origin: process.env.FRONTEND_ORIGIN,
-        credentials: true
-    })
-)
-app.use('/', userRouter);
+  cors({
+    origin: process.env.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+app.use("/", userRouter);
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT;
 
 (async () => {
-    await connectDB();
-    server.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
+  await connectDB();
+  server.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
 })();
