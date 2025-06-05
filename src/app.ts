e5142b873fbe6f7820a375cc2b521b3724792ff3
syam-ts <syamnandhu3@gmail.com>
2025-06-05 20:17:01 +0530

    fix: removed unwanted codes from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,30 @@
-import express from "express";
+import { connectDB } from "./infrastructure/database/db";
+import indexRouter from "./infrastructure/http/routes/index";
+import cookieparser from "cookie-parser";
 import http from "node:http";
 import path from "node:path";
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config({
   path: path.resolve(__dirname, "../env"),
 });
-import cors from "cors";
-import { connectDB } from "./infrastructure/database/db";
-<<<<<<< HEAD
-import indexRouter from './infrastructure/http/routes/index';
-import cookieparser from 'cookie-parser';
- 
-=======
-import userRouter from "./infrastructure/http/routes/userRouter";
-import cookieparser from "cookie-parser";
->>>>>>> a79778edd5f248f3be44b6beb6ff156c13fd7bb4
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieparser());
 app.use(
-<<<<<<< HEAD
-    cors({
-        origin: process.env.FRONTEND_ORIGIN,
-        credentials: true
-    })
-)
-app.use('/', indexRouter);
-=======
   cors({
-    origin: 'https://readora-puce.vercel.app',
+    origin: process.env.FRONTEND_ORIGIN,
     credentials: true,
   })
 );
-app.use("/", userRouter);
+app.use("/", indexRouter);
 
->>>>>>> a79778edd5f248f3be44b6beb6ff156c13fd7bb4
 const server = http.createServer(app);
 
 const PORT = process.env.PORT;
-
 
 (async () => {
   await connectDB();
